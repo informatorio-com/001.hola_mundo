e6cca46c0bdc39de5695ec70e2be7cfde3d2dde3
xlmriosx <xlmriosx@gmail.com>
2024-07-18 19:36:53 -0300

    add scripts and workflow to check integrity

---
+++ .github/verify/app/hola_mundo.py
@@ -0,0 +1,12 @@
+# Hola Mundo! [Python]
+# ---------------------------
+# Autor: Lucas M. Rios
+# Version: 2.0
+
+# Descripcion:
+# Programa creado para ensayar el correcto funcionamiento
+# del entorno de instalación Python
+
+# Hace que la funcion devuelva un print con "Hola Mundo!!!"
+def hola_mundo():
+    return print("Hola Mundo!!!")
