813229373be33f62cd6e7257179c5122df682840
xlmriosx <xlmriosx@gmail.com>
2024-07-18 19:05:47 -0300

    intial commit

---
+++ .github/tests/hola_mundo_test.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import pytest
+
+# Add the parent directory of hola_mundo.py to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from hola_mundo import hola_mundo
+
+def test_hola_mundo(capsys):
+    result = hola_mundo()
+    captured = capsys.readouterr()
+    assert captured.out == "Hola Mundo!!!\n"
+    assert result is None
---
+++ README.md
@@ -0,0 +1,8 @@
+![Informatorio banner](.github/assets/informatorio.jpg)
+Web: [Informatorio](http://campus-informatorio.com.ar)
+
+# Hola Mundo! [Python]
+Programa creado para ensayar el correcto funcionamiento del entorno de instalación Python
+
+# Consultas
+xlmriosx@gmail.com.ar
